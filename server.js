4a9674ae332d2d3e95169b4261e521aa0f19dcc3
Ayush2705 <ayush.agr.27.5@gmail.com>
2023-12-27 18:50:10 +0530

    change addproduct

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/donators', async (req, res) => {
 
 
 // Endpoint to add products
-app.post('/addproduct', async (req, res) => {
+app.post('/addProduct', async (req, res) => {
   try {
     console.log('Request received for /addproduct');
     const { name_in_english,name_in_hindi,type,cost } = req.body;
